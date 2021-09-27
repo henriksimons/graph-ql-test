2fbf91bdf8ab7989444ecb52fd5af41ad21184ac
henriksimons <h3nkes@gmail.com>
2021-09-27 09:44:23 +0200

    Add spring initializer project

---
+++ src/main/java/my/appgraphql/AppGraphQlApplication.java
@@ -0,0 +1,13 @@
+package my.appgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/appgraphql/AppGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package my.appgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
